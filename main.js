2778a6089a363302af1e06697e86b4f0bf1e7588
erik <erikparisva@gmail.com>
2020-06-18 16:14:47 +0300

    changes

--- main.js
+++ main.js
@@ -15,15 +15,14 @@ fetch("https://restcountries.eu/rest/v2/all")
 function initialize(countriesData) {
   countries = countriesData;
   let options = "";
-
   countries.forEach(country => options+=`<option value="${country.alpha3Code}">${country.name}</option>`);
   countriesList.innerHTML = options;
   countriesList.selectedIndex = Math.floor(Math.random()*countriesList.length);
   displayCountryInfo(countriesList[countriesList.selectedIndex].value);
 }
 
-function displayCountryInfo(countryByName) {
-  const countryData = countries.find(country => country.name === countryByName);
+function displayCountryInfo(countryByAlpha3Code) {
+  const countryData = countries.find(country => country.alpha3Code === countryByAlpha3Code);
   document.querySelector("#flag-container img").src = countryData.flag;
   document.querySelector("#flag-container img").alt = `Flag of ${countryData.name}`;  
   document.getElementById("capital").innerHTML = countryData.capital;
